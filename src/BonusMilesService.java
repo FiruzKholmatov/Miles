f06e46b6144cac6bd07996bf68cce15391f8317c
firuz kholmatov <firuzkholmatov@yandex.ru>
2021-09-19 20:12:57 +0300

    upload

---
+++ src/BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+            public int calculate(int amount) {
+            int cost = 20;
+            int miles = amount / cost;
+            return miles;
+        }
+
+}
